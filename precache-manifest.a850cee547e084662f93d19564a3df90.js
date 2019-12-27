de6e4dbe163b8cfa919299456fffd3d767a1dcef
Skyggfisk <mahasi93@hotmail.com>
2019-12-27 20:29:32 +0100

    Updates

---
+++ precache-manifest.a850cee547e084662f93d19564a3df90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50de802fa9b9dc67a61e30ce8567714b",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "14df836c2a208ced8bd3",
+    "url": "/react-todo-app/static/css/main.0ab35acc.chunk.css"
+  },
+  {
+    "revision": "e60c4cc7219dd2512e45",
+    "url": "/react-todo-app/static/js/2.a1bf6cff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-app/static/js/2.a1bf6cff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14df836c2a208ced8bd3",
+    "url": "/react-todo-app/static/js/main.48475961.chunk.js"
+  },
+  {
+    "revision": "6b17cf08a32aff533625",
+    "url": "/react-todo-app/static/js/runtime-main.bd41c713.js"
+  }
+]);
